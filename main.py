7d69ed6e57e09757e46ddc85d2ca3e67809390b0
Andres Palma <palma.castaneda030@gmail.com>
2024-07-25 23:14:07 -0500

    minuto 33:04

---
+++ main.py
@@ -0,0 +1,10 @@
+suits = ['spades', 'clubs', 'hearts', 'diamonds ']
+ranks  = ['A', '2', '3', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+suit = "hearts"
+rank = "K"
+value = 10
+print( f"Your card is:{rank} of  {suit}")
+print(rank)
+suits.append('snakes')
+for suit in suits:
+    print(suit)
